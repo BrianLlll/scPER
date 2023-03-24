b8b30c4ef1b5cee66b92c1d9d0a7c5006ec0b541
Brian <46977756+BrianLlll@users.noreply.github.com>
2023-03-24 15:58:55 -0500

    Add files via upload

---
+++ code/preprocess_data.R
@@ -0,0 +1,45 @@
+library(Rmagic)
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)<2) {
+  stop("Both referece and mixture files must be supplied (input files)", call.=FALSE)
+} 
+
+sc_df<-read.csv(args[1],row.names = 1,sep='\t')
+
+bulk<-read.csv(args[2],row.names = 1,sep='\t')
+
+###identify the overlap genes between reference and bulk mixture
+
+overlap<-intersect(rownames(sc_df),rownames(bulk))
+
+sc_df<-sc_df[overlap,]
+
+####imputation using MAGIC
+
+MAGIC_data <- magic(t(sc_df))
+
+sc_df_impute<-t(MAGIC_data$result)
+
+##select top5000 most variable genes
+
+apply(sc_df_impute, 1, var) -> rowVar
+
+names(rowVar[order(rowVar,decreasing = TRUE)][1:5000])->mad.genes
+
+mad5000= sc_df_impute[mad.genes,] ### gene by sample
+
+
+write.csv(mad5000,'reference_top5k_imputation.csv',quote=FALSE)
+
+
+bulk_overlap<-bulk[mad.genes,]
+
+write.csv(bulk_overlap,'bulk_5k_genes_matched.csv',quote=FALSE)
+
+
+
+
+
+
